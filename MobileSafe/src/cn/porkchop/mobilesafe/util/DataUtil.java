233b1c91ac605213ae5111782270b7b3dc637fd7
qq714772696 <714772696@qq.com>
2017-06-17 23:38:29 +0800

    添加方便debug的工具类

---
+++ MobileSafe/src/cn/porkchop/mobilesafe/util/DataUtil.java
@@ -0,0 +1,13 @@
+package cn.porkchop.mobilesafe.util;
+
+import java.util.TooManyListenersException;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class DataUtil {
+	public static void showDebugInfo(Context context, String info) {
+		Toast.makeText(context, info, 0).show();
+		System.err.println("info");
+	}
+}
