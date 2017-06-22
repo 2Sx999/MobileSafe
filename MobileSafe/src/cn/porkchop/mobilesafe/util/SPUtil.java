61f542ff3d0e7bb84920639e05964b514d6d73aa
qq714772696 <714772696@qq.com>
2017-06-22 10:58:59 +0800

    新增String类型的存取方法

--- MobileSafe/src/cn/porkchop/mobilesafe/util/SPUtil.java
+++ MobileSafe/src/cn/porkchop/mobilesafe/util/SPUtil.java
@@ -14,10 +14,26 @@ public class SPUtil {
 		editor.apply();
 	}
 
-	public static boolean getBoolean(Context context, String fileName, String key,
-			boolean defaultValue) {
+	public static boolean getBoolean(Context context, String fileName,
+			String key, boolean defaultValue) {
 		SharedPreferences sp = context.getSharedPreferences(fileName,
 				Context.MODE_PRIVATE);
 		return sp.getBoolean(key, defaultValue);
+	}
+
+	public static void putString(Context context, String fileName, String key,
+			String value) {
+		SharedPreferences sp = context.getSharedPreferences(fileName,
+				Context.MODE_PRIVATE);
+		Editor editor = sp.edit();
+		editor.putString(key, value);
+		editor.apply();
+	}
+
+	public static String getString(Context context, String fileName,
+			String key, String defaultValue) {
+		SharedPreferences sp = context.getSharedPreferences(fileName,
+				Context.MODE_PRIVATE);
+		return sp.getString(key, defaultValue);
 	}
 }
