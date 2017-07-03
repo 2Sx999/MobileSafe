d5bc8766bb8236c6c3c0229d9847c16dfc6106e1
qq714772696 <714772696@qq.com>
2017-07-03 18:01:52 +0800

    添加了方法

--- MobileSafe/src/cn/porkchop/mobilesafe/util/DataUtil.java
+++ MobileSafe/src/cn/porkchop/mobilesafe/util/DataUtil.java
@@ -8,7 +8,10 @@ import android.widget.Toast;
 public class DataUtil {
 	public static void showDebugInfo(Context context, String info) {
 		Toast.makeText(context, info, 0).show();
-		System.err.println("info");
+		System.out.println(info);
+	}
+	public static void showToast(Context context, String info) {
+		Toast.makeText(context, info, 0).show();
 	}
 	
 }
