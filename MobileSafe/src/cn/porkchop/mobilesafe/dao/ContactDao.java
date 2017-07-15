52c5c5555f3ce6c8fa408556e8ba058cbeafff08
qq714772696 <714772696@qq.com>
2017-07-15 21:19:08 +0800

    关闭cursor

--- MobileSafe/src/cn/porkchop/mobilesafe/dao/ContactDao.java
+++ MobileSafe/src/cn/porkchop/mobilesafe/dao/ContactDao.java
@@ -10,34 +10,39 @@ import android.net.Uri;
 
 import cn.porkchop.mobilesafe.model.Contact;
 
-
 public class ContactDao {
 	public List<Contact> getAllContacts(Context context) {
 		List<Contact> list = new ArrayList<Contact>();
 		ContentResolver contentResolver = context.getContentResolver();
-		//raw_contacts表
-		Uri contactUri = Uri.parse("content://com.android.contacts/raw_contacts");
-		//data表
+		// raw_contacts表
+		Uri contactUri = Uri
+				.parse("content://com.android.contacts/raw_contacts");
+		// data表
 		Uri dataUri = Uri.parse("content://com.android.contacts/data");
-		Cursor contactCursor = contentResolver.query(contactUri, new String[]{"contact_id"}, null, null, null);
-		while(contactCursor.moveToNext()){
+		Cursor contactCursor = contentResolver.query(contactUri,
+				new String[] { "contact_id" }, null, null, null);
+		while (contactCursor.moveToNext()) {
 			Contact contact = new Contact();
 			String contactId = contactCursor.getString(0);
-			if(contactId == null){
+			if (contactId == null) {
 				continue;
 			}
-			Cursor dataCursor = contentResolver.query(dataUri, new String[]{"data1","mimetype"}, "raw_contact_id=?", new String[]{contactId}, null);
-			while(dataCursor.moveToNext()){
+			Cursor dataCursor = contentResolver.query(dataUri, new String[] {
+					"data1", "mimetype" }, "raw_contact_id=?",
+					new String[] { contactId }, null);
+			while (dataCursor.moveToNext()) {
 				String mimeType = dataCursor.getString(1);
 				String data1 = dataCursor.getString(0);
-				if("vnd.android.cursor.item/phone_v2".equals(mimeType)){
+				if ("vnd.android.cursor.item/phone_v2".equals(mimeType)) {
 					contact.setPhone(data1);
-				}else if("vnd.android.cursor.item/name".equals(mimeType)){
+				} else if ("vnd.android.cursor.item/name".equals(mimeType)) {
 					contact.setName(data1);
 				}
 			}
+			dataCursor.close();
 			list.add(contact);
 		}
+		contactCursor.close();
 		return list;
 	}
 }
